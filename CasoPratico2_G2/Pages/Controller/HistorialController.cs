042fb7de235bcf65995f0babba445a6a866bd10e
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-12-07 17:52:44 -0600

    Model y Controller de Historial

---
+++ CasoPratico2_G2/Pages/Controller/HistorialController.cs
@@ -0,0 +1,31 @@
+﻿using CasoPratico2_G2.Pages.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasoPratico2_G2.Pages.Controller
+{
+    public class HistorialController : ControllerBase
+    {
+        private readonly List<HistorialModel> historial;
+
+        public HistorialController()
+        {
+            historial = new List<HistorialModel>();
+        }
+
+        public void AgregarRegistro(string juegoId, string jugador1, string jugador2, string ganador, TimeSpan duracion)
+        {
+            var registro = new HistorialModel
+            {
+                JuegoId = juegoId,
+                Jugador1 = jugador1,
+                Jugador2 = jugador2,
+                Ganador = ganador,
+                Duracion = duracion
+            };
+
+            historial.Add(registro);
+        }
+
+        public List<HistorialModel> ObtenerHistorial() => historial;
+    }
+}
---
+++ CasoPratico2_G2/Pages/Models/HistorialModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CasoPratico2_G2.Pages.Models
+{
+    public class HistorialModel
+    {
+        public string JuegoId { get; set; }
+        public string Jugador1 { get; set; }
+        public string Jugador2 { get; set; }
+        public string Ganador { get; set; }
+        public TimeSpan Duracion { get; set; }
+    }
+}
